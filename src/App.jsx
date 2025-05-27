11f9f84346fe97801ab18b9286bf12b65dd52e8c
Anshul Bhaduria <anshulsingh80@gmail.com>
2025-05-28 00:58:18 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import ProductListingPage from "./features/products/productListingPage";
+import ProductListingPage from "./features/products/ProductListingPage";
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 
 function App() {
